876a70e2fb058e1b325ca3692b98f20095cfa8dd
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-05-30 07:01:51 -0600

    add notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_30_122747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "notes", force: :cascade do |t|
+    t.text "body"
+    t.bigint "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["person_id"], name: "index_notes_on_person_id"
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "hair_color"
+    t.string "eye_color"
+    t.string "gender"
+    t.boolean "alive"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "notes"
+  end
+
+  add_foreign_key "notes", "people"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 gender = ["male", "female"]
+note = ["Cool", "Awesome", "Great", "Fun", "Smart"]
 
 25.times do
   Person.create(
@@ -8,5 +9,8 @@ gender = ["male", "female"]
     eye_color: Faker::Color.color_name,
     gender: gender.sample,
     alive: Faker::Boolean.boolean
+  )
+  Note.create(
+    body: note.sample
   )
 end
